9f8ad088cf5206b079a2d242277221ef24b165a8
Harshita Chadha <54230749+hersheychadha@users.noreply.github.com>
2021-01-20 17:55:23 +0530

    Update settings.py

--- cybernation/settings.py
+++ cybernation/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
